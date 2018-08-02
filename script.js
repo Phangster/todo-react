5a3a473ad6ca2438b89b5ef1df323e245852f03d
Phangster <mynamebryanphang@gmail.com>
2018-08-03 04:07:35 +0800

    Completed deliverables

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ class List extends React.Component {
   constructor(){
     super()
     this.changeHandler = this.changeHandler.bind( this );
+    this.submitHandler = this.submitHandler.bind( this );
+
+
   }
 
   state = {
@@ -12,16 +15,33 @@ class List extends React.Component {
   changeHandler(event){
     this.setState({word:event.target.value});
     console.log("change", event.target.value);
+    
+  }
+  submitHandler(event){
+    this.state.list.push(this.state.word);
+    this.state.word="";
+    this.setState({word:event.target.value})
+    
   }
 
   render() {
       // render the list with a map() here
-
+      console.log(this.state.list)
       console.log("rendering");
       return (
         <div className="list">
-          <input onChange={this.changeHandler} value={this.state.word}/>
-          <button>add item</button>
+          <h1>TO DO LIST</h1><br />
+          <textarea onChange={this.changeHandler} value={this.state.word}/><br />
+          <button onClick={this.submitHandler}>add note</button>
+            <ul class='pointer'>
+            {this.state.list.map(function(message, index){
+              return (
+                <div>
+                <li key={index}>{message}</li>
+                </div>
+              );
+            })}
+            </ul>   
         </div>
       );
   }
